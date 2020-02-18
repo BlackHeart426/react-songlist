d5ca2f3503e37222902dfbb3dd639e7165803ba8
BlackHeart <valepa007@gmail.com>
2020-02-18 22:04:02 +0300

    redux Auth

--- src/companents/Auth/Auth.js
+++ src/companents/Auth/Auth.js
@@ -1,33 +1,75 @@
 import React, { Component } from "react";
 import {Button} from "react-bootstrap";
 import {connect} from "react-redux";
+import {auth, logout} from "../../store/action/auth";
 
 class Auth extends Component {
 
+    loginHandler = () => {
+        this.props.auth(
+            'val@gmail.com',
+            1234567,
+            true
+        )
+    }
+
+    registerHandler = () => {
+        this.props.auth(
+            'val@gmail.com',
+            1234567,
+            false
+        )
+
+    }
+
+    logoutHandler = () => {
+        this.props.logout()
+
+    }
 
     render() {
         return (
             <>
-                <p>{this.props.counter}</p>
-                <Button onClick={this.props.onAdd} className="nav-dark">asd</Button>
+
+                <Button
+                    onClick={this.registerHandler}
+                    type="submit"
+                    className="btn-dark"
+                >
+                    Регистрация
+                </Button>
+                <Button
+                    onClick={this.loginHandler}
+                    className="btn-light"
+                    type="submit"
+                >
+                    Вход
+                </Button>
+                <Button
+                    onClick={this.logoutHandler}
+                    className="btn-light"
+                    type="submit"
+                >
+                    Выход
+                </Button>
             </>
         )
     }
 
 }
 
-function mapStateToProbs(state) {
+function mapStateToProps(state) {
     return {
         counter: state.counter
     }
 
 }
 
-function mapDispatchToProbs(dispatch) {
+function mapDispatchToProps(dispatch) {
     return {
-        onAdd: () => dispatch({type: 'ADD'})
+        auth: (email, password, isLogin) => dispatch(auth(email, password, isLogin)),
+        logout: () => dispatch(logout)
     }
-
 }
 
-export default connect(mapStateToProbs, mapDispatchToProbs)(Auth)
+export default connect(mapStateToProps, mapDispatchToProps)(Auth)
--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {createStore} from 'redux'
-import rootReducer from "./reducers/rootReducer";
+import {applyMiddleware, createStore} from 'redux'
+import rootReducer from "./store/reducers/rootReducer";
 import {Provider} from 'react-redux'
+import thunk from "redux-thunk";
 
-const store = createStore(rootReducer);
+const store = createStore(
+    rootReducer,
+    applyMiddleware(thunk)
+);
 
 const app = (
     <Provider store={store}>
