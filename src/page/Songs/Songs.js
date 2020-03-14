c81fd96bc354836dcff55931ebbbb63418515a94
BlackHeart <valepa007@gmail.com>
2020-03-14 15:37:34 +0300

    firebase config

--- src/page/Songs/Songs.js
+++ src/page/Songs/Songs.js
@@ -3,6 +3,16 @@ import TablePagination from '../../companents/TablePagination/ComponentTablePagi
 import {PTBSongs} from "./PTBSongs/PTBSongs";
 import {SongsContext} from "../../contex/module/songs/songsContext";
 import * as shortid from "shortid";
+import Axios, {AxiosInstance as axios} from "axios";
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import * as firebase from "firebase/app";
+
+// If you enabled Analytics in your project, add the Firebase SDK for Analytics
+import "firebase/analytics";
+
+// Add the Firebase products that you want to use
+import "firebase/auth";
+import "firebase/firestore";
 
 export function createData(title, artist, timesPlayed, lastPlayed, tags, action) {
     return {title, artist, timesPlayed, lastPlayed, tags, action}
@@ -90,7 +100,27 @@ export const Songs = () => {
     let filtered = [];
     useEffect(() => {
         setSongData(songList)
+        const name = {name:'fghfghfg', data:[]};
         localStorage.setItem('listSongs', JSON.stringify(songList))
+        const firebaseRef = firebase.database().ref("chat").child('aaa').child('ccc');
+        firebaseRef.set({
+            text : 'bbb'
+        });
+        var firebaseConfig = {
+            apiKey: "api-key",
+            authDomain: "project-id.firebaseapp.com",
+            databaseURL: "https://project-id.firebaseio.com",
+            projectId: "project-id",
+            storageBucket: "project-id.appspot.com",
+            messagingSenderId: "sender-id",
+            appId: "app-id",
+            measurementId: "G-measurement-id",
+        };
+        firebase.initializeApp(firebaseConfig);
+        Axios.post('https://song-list-95d78.firebaseio.com/listSongsTest.json', name )
+        .then(response => {
+            console.log(response)
+        })
     },[])
 
 
